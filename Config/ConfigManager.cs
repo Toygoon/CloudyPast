e6b77a58fb44d1ead3e4b943796ad252a2d3efc7
임정민 <toygoon@gmail.com>
2023-11-16 13:38:44 +0900

    [Add] Config 관련 패키지, 기능 추가

---
+++ Config/ConfigManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace CloudyPast.Config {
+    internal class ConfigManager {
+        public bool Exists() {
+            return System.IO.File.Exists(Const.CONFIG_FILE);
+        }
+
+        public void Generate(bool force = false) {
+            if (Exists() && !force)
+                return;
+
+            AppSettingsReader ar = new AppSettingsReader();
+            System.Configuration.Configuration c = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+
+            c.AppSettings.Settings["Dir"].Value = "C:\\a";
+
+            c.Save(ConfigurationSaveMode.Modified);
+            ConfigurationManager.RefreshSection("appSettings");
+
+            Console.WriteLine(c.AppSettings.Settings["Dir"]);
+        }
+    }
+}
