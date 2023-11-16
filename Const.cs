da6063a4eff27e53d0f719f1770342b27b5226e4
임정민 <toygoon@gmail.com>
2023-11-16 13:38:31 +0900

    [Add] 상수 클래스 추가

---
+++ Const.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudyPast {
+    internal class Const {
+        public const string CONFIG_FILE = "App.config";
+        public const string WINDOW_TITLE = "CloudyPast";
+        public const bool DEBUG_MODE = true;
+    }
+
+
+    static class NativeMethods {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+
+        public static extern bool AllocConsole();
+    }
+}
